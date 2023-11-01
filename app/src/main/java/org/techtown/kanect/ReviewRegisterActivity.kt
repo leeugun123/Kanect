37469a889b89201402863e039e3742a53c029f13
leeugun123 <leeugun123@naver.com>
2023-11-01 17:55:16 +0900

    ReviewRegister MVVM 패턴 적용 및 코드 최적화

--- app/src/main/java/org/techtown/kanect/ReviewRegisterActivity.kt
+++ app/src/main/java/org/techtown/kanect/ReviewRegisterActivity.kt
@@ -3,16 +3,20 @@ package org.techtown.kanect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import org.techtown.kanect.Data.UserIntel
+import org.techtown.kanect.Object.GetTime
 import org.techtown.kanect.Object.UserKakaoInfo
+import org.techtown.kanect.ViewModel.ReviewRegisterViewModel
 import org.techtown.kanect.databinding.ActivityReviewRegisterBinding
 
 class ReviewRegisterActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityReviewRegisterBinding
+    private lateinit var reviewRegisterViewModel : ReviewRegisterViewModel
     private lateinit var cafeName : String
     private lateinit var database : FirebaseDatabase
     private lateinit var cafeReviewRef : DatabaseReference
@@ -22,31 +26,49 @@ class ReviewRegisterActivity : AppCompatActivity() {
         binding = ActivityReviewRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+        reviewRegisterViewModel = ViewModelProvider(this).get(ReviewRegisterViewModel::class.java)
+
         reviewRegisterInit()
 
 
         binding.registerBut.setOnClickListener {
 
-            if(binding.reviewEdit.text.toString().isNotEmpty()){
+            val reviewText = binding.reviewEdit.text.toString()
 
-                val newUserRef : DatabaseReference = cafeReviewRef.child(cafeName).push()
+            if (reviewText.isNotEmpty()) {
 
-                newUserRef.setValue(UserIntel(UserKakaoInfo.userImg , UserKakaoInfo.userName , binding.reviewEdit.text.toString(), org.techtown.kanect.Object.GetTime.getCurrentDate()))
-                    .addOnSuccessListener {
+                reviewRegisterViewModel.registerReview(
+                    cafeReviewRef,
+                    cafeName,
+                    UserKakaoInfo.userImg,
+                    UserKakaoInfo.userName,
+                    reviewText,
+                    GetTime.getCurrentDate()
+                )
 
-                        Toast.makeText(this,"후기 작성 완료",Toast.LENGTH_SHORT).show()
-                        finish()
+            } else {
 
-                    }
-                    .addOnFailureListener {
-                        Toast.makeText(this,"후기 작성 실패. 네트워크 오류 ",Toast.LENGTH_SHORT).show()
-                    }
+                Toast.makeText(this, "적어도 한 글자 이상 작성해주세요.", Toast.LENGTH_SHORT).show()
 
-            }else{
-                Toast.makeText(this,"적어도 한 글자 이상 작성해주세요.",Toast.LENGTH_SHORT).show()
             }
 
         }//후기 쓰기
+
+
+        reviewRegisterViewModel.registerStatus.observe(this) { isRegistered ->
+
+            if (isRegistered) {
+                Toast.makeText(this, "후기 작성 완료", Toast.LENGTH_SHORT).show()
+                finish()
+
+            } else {
+
+                Toast.makeText(this, "후기 작성 실패. 네트워크 오류", Toast.LENGTH_SHORT).show()
+
+            }
+
+        }
 
 
     }
---
+++ app/src/main/java/org/techtown/kanect/ViewModel/ReviewRegisterViewModel.kt
@@ -0,0 +1,31 @@
+package org.techtown.kanect.ViewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.database.DatabaseReference
+import org.techtown.kanect.Data.UserIntel
+
+class ReviewRegisterViewModel : ViewModel() {
+
+    private val _registerStatus = MutableLiveData<Boolean>()
+    val registerStatus: LiveData<Boolean>
+        get() = _registerStatus
+
+    fun registerReview(cafeReviewRef : DatabaseReference, cafeName: String, userImg: String, userName: String, reviewText: String, currentDate: String) {
+
+        val newUserRef: DatabaseReference = cafeReviewRef.child(cafeName).push()
+
+        newUserRef.setValue(UserIntel(userImg, userName, reviewText, currentDate))
+            .addOnSuccessListener {
+                _registerStatus.value = true
+            }
+            .addOnFailureListener {
+                _registerStatus.value = false
+            }
+
+
+    }
+
+
+}
